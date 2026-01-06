9d394d694b3d79bef4347d81b3a11989ebad7e93
Jorge R. Romero <jorirovi@icloud.com>
2026-01-06 09:20:36 +0100

    culminacion de la Section2 de la pagina

--- js/app.js
+++ js/app.js
@@ -2,8 +2,15 @@ const xhr = new XMLHttpRequest();
 const API_KEY = '8ae9539a9c5439a57658bbb0fb62e8ab';
 const category = 'technology';
 const d = document;
+//script para el main
 const btn = d.getElementById('noticias');
-
+const txt = d.getElementById('texto');
+const parr1 = d.getElementById('p1');
+const parr2 = d.getElementById('p2');
+const imagen = d.getElementById('imgqs');
+txt.textContent = "Empresa líder en soluciones 360º dentro de la industria de tecnologia, el entretenimiento y la movilidad corporativa.";
+parr1.innerHTML = "Transformamos la manera en que las <strong>empresas</strong> y <strong>emprendedores</strong> construyen su presencia digital. Somos una compañía especializada en servicios de <strong>Hosting</strong>, <strong>desarrollo web</strong>, <strong>soluciones en la nube</strong> y <strong>arquitectura tecnológica personalizada</strong>, orientada a ofrecer rendimiento, seguridad y escalabilidad en cada proyecto."
+parr2.innerHTML = "Brindar una infraestructura <strong>confiable </strong>y <strong>moderna</strong>, combinada con un equipo experto en innovación digital, para que nuestros clientes puedan enfocarse en hacer crecer su negocio mientras nosotros nos ocupamos del entorno tecnológico que lo impulsa.";
 const cargarNoticias = () => {
     xhr.addEventListener('readystatechange', () => {
         if (xhr.readyState !== 4) return;
