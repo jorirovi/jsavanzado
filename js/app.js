53ce0ebfe7684915ecbb0dd6642c847deaf5e89a
Jorge R. Romero <jorirovi@icloud.com>
2026-01-07 07:00:46 +0100

    Finalizacion del Pie de Pagina y Noticias

--- js/app.js
+++ js/app.js
@@ -11,17 +11,38 @@ const imagen = d.getElementById('imgqs');
 txt.textContent = "Empresa líder en soluciones 360º dentro de la industria de tecnologia, el entretenimiento y la movilidad corporativa.";
 parr1.innerHTML = "Transformamos la manera en que las <strong>empresas</strong> y <strong>emprendedores</strong> construyen su presencia digital. Somos una compañía especializada en servicios de <strong>Hosting</strong>, <strong>desarrollo web</strong>, <strong>soluciones en la nube</strong> y <strong>arquitectura tecnológica personalizada</strong>, orientada a ofrecer rendimiento, seguridad y escalabilidad en cada proyecto."
 parr2.innerHTML = "Brindar una infraestructura <strong>confiable </strong>y <strong>moderna</strong>, combinada con un equipo experto en innovación digital, para que nuestros clientes puedan enfocarse en hacer crecer su negocio mientras nosotros nos ocupamos del entorno tecnológico que lo impulsa.";
-const cargarNoticias = () => {
+//script para la carga de Noticias
+const notiGrid = d.getElementById('notigrid');
+try {
     xhr.addEventListener('readystatechange', () => {
-        if (xhr.readyState !== 4) return;
-        if (xhr.status >= 200 && xhr.status < 300) {
-            const json = JSON.parse(xhr.responseText);
-            console.log(json)
+        if(xhr.readyState !== 4) return;
+        if(xhr.status >= 200 && xhr.status < 300){
+            const noticias = JSON.parse(xhr.responseText);
+            notiGrid.innerHTML = noticias.articles.map(noticia => {
+                const notiimg    = noticia.image || "https://picsum.dev/300/200";
+                const notititulo = noticia.title ?? "Sin Titulo";
+                const notidesc   = noticia.description ?? "";
+                const notiurl    = noticia.url ?? "";
+                return `
+                    <article class="noti-cards">
+                        <img src="${notiimg}" alt="${notititulo}"></img>
+                        <div class="contenido">
+                            <h3>${notititulo}</h3>
+                            <p>${notidesc}</p>
+                            <a href="${notiurl}" target="_blank" rel="noopener noreferrer">Ver Noticia</a>
+                        </div>
+                    </article>
+                `
+            }).join("");
+        }
+        else {
+            console.error(xhr.status);
+            notiGrid.innerHTML = '<p>No se cargron las noticias</p>'
         }
     });
-
-    xhr.open('GET','https://gnews.io/api/v4/top-headlines?category=' + category + '&lang=en&country=us&max=10&apikey=' + API_KEY);
+    xhr.open('GET', './data/noticias.json'); //AQUI IRA EL URL DE LA API
     xhr.send();
+} catch (err) {
+    console.error(err);
+    notiGrid.innerHTML = '<p>No se pudieron cargar las noticias.</p>';
 }
-
-btn.addEventListener('click', cargarNoticias, true);
